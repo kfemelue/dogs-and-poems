8d044eff0c11f5cf2944efeea382e09226661da9
frank-dev <frank-dev@Mitchells-MBP-2.localdomain>
2024-06-25 14:43:21 -0500

    Create Express Server app, Begin building data models for dogs and poems

---
+++ src/models/poetryDog.js
@@ -0,0 +1,18 @@
+class Poem {
+    contructor(dog, title, author, text){
+        /**
+         * @param {String} title The title of the poem
+         * @param {String} author The author of the poem
+         * @param {String} text The text of the poem
+         */
+    }
+}
+
+class Dog {
+    constructor(){
+        /**
+         * 
+         */
+    }
+
+}
