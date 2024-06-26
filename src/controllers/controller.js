9046729d0a7f04173f447a622f35dd8866aa85e7
frank-dev <frank-dev@Mitchells-MBP-2.localdomain>
2024-06-26 14:19:37 -0500

    Refactor Controller. Render Data in html ejs view. TODO: add bootstrap styling

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -3,18 +3,11 @@ const DogWithPoem = require("../models/poetryDog");
 
 let resultObj = {}
 
-
-async function getDogPoem (req, res, next) {
-    let obj = {};
-    await fetchDog()
-    .then(img =>{
-    })
-
-    await fetchPoem()
-    .then(poem=>{
-    })
-    //let result = new DogWithPoem(myObj.img, myObj.title, myObj.author, myObj.lines);
-    res.status(200).send(resultObj)
+function getDogPoem (req, res, next) {
+    const dogAndPoem = new DogWithPoem(resultObj.img, resultObj.title, resultObj.author, resultObj.lines);
+    fetchDog();
+    fetchPoem();
+    res.status(200).render("index", { resultObj: resultObj });
 }
 
 async function fetchDog() {
@@ -26,6 +19,7 @@ async function fetchDog() {
         resultObj.img = img;
         return img
     }).catch(error => {
+        resultObj.img = "https://media.istockphoto.com/id/486150666/photo/404-error.jpg?s=612x612&w=0&k=20&c=0gyIrlpbS04D0S0d9ED_2tjE3-L5lMnVamyuohZ8TKQ="
         console.error("Something Went wrong with the Dog Image API. Error: ", error);
     });
 }
@@ -37,14 +31,11 @@ async function fetchPoem() {
     .then(data => {
         //filter poems by linecount with a maximum of 25 lines
         data = data.filter((item)=> Number(item.linecount)<=25)
-
         //select a random poem
-        let poem = data[Math.floor(Math.random()*data.length)];
-
+        const poem = data[Math.floor(Math.random()*data.length)];
         resultObj.title = poem.title;
         resultObj.author = poem.author;
         resultObj.lines = poem.lines;
-
         return poem;
     })
     .catch(error => {
